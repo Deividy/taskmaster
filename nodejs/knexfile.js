ac841616acb9e0c9a258f4c1ecff1560a6a079e4
Leo Rodrigues <leo.rf.dev@gmail.com>
2021-10-03 18:39:00 -0300

    chore: add knex config file

---
+++ nodejs/knexfile.js
@@ -0,0 +1,14 @@
+require('./dotenv');
+
+module.exports = {
+    client: 'pg',
+    connection: {
+        host: process.env.PSQL_HOST,
+        user: process.env.PSQL_USER,
+        password: process.env.PSQL_PASSWORD,
+        database: process.env.PSQL_DATABASE
+    },
+    migrations: {
+        directory: './src/migrations'
+    }
+}
